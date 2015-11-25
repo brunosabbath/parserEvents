2065819aa3136668a3465d5680fae33d5b4ff5e1
Bruno <Bruno@192.168.25.4>
2015-11-25 09:17:50 -0200

    gh

---
+++ src/main/java/com/bruno/parser/DateHourJournal.java
@@ -0,0 +1,15 @@
+package com.bruno.parser;
+
+public class DateHourJournal {
+	
+	public static void main(String[] args) {
+		
+		String date = "Thu, Oct 22 2015, 1:30 pm CDT";
+		
+		String d[] = date.split(",");
+		
+		System.out.println("pow");
+		
+	}
+	
+}
